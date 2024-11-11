073d2172c7674b98a5d1bd172d0d6a53f4d06c3c
Chris Sells <csells@sellsbrothers.com>
2024-11-10 16:18:38 -0800

    working (needed a background color)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'dart:typed_data';
+import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_vertexai/firebase_vertexai.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ai_toolkit/flutter_ai_toolkit.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:signature/signature.dart';
 
@@ -35,9 +37,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   // initialize the signature controller
   final _controller = SignatureController(
-    penStrokeWidth: 1,
+    penStrokeWidth: 2,
     penColor: Colors.red,
-    exportBackgroundColor: Colors.transparent,
+    exportBackgroundColor: Colors.white,
     exportPenColor: Colors.black,
     onDrawStart: () => debugPrint('onDrawStart called!'),
     onDrawEnd: () => debugPrint('onDrawEnd called!'),
@@ -62,7 +64,79 @@ class _HomePageState extends State<HomePage> {
     super.dispose();
   }
 
-  Future<void> exportImage(BuildContext context) async {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: const Text('Vertex AI Quickdraw!')),
+        body: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Signature(
+                  key: const Key('signature'),
+                  controller: _controller,
+                  backgroundColor: Colors.grey[300]!,
+                ),
+              ),
+            ),
+            Text(_controller.isEmpty
+                ? "Signature pad is empty"
+                : "Signature pad is not empty"),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            decoration: const BoxDecoration(color: Colors.black),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                IconButton(
+                  key: const Key('exportPNG'),
+                  icon: const Icon(Icons.image),
+                  color: Colors.blue,
+                  onPressed: _exportImage,
+                  tooltip: 'Export Image',
+                ),
+                IconButton(
+                  key: const Key('exportSVG'),
+                  icon: const Icon(Icons.share),
+                  color: Colors.blue,
+                  onPressed: _exportSVG,
+                  tooltip: 'Export SVG',
+                ),
+                IconButton(
+                  icon: const Icon(Icons.undo),
+                  color: Colors.blue,
+                  onPressed: _undo,
+                  tooltip: 'Undo',
+                ),
+                IconButton(
+                  icon: const Icon(Icons.redo),
+                  color: Colors.blue,
+                  onPressed: _redo,
+                  tooltip: 'Redo',
+                ),
+                IconButton(
+                  key: const Key('clear'),
+                  icon: const Icon(Icons.clear),
+                  color: Colors.blue,
+                  onPressed: _clear,
+                  tooltip: 'Clear',
+                ),
+                IconButton(
+                  icon: const Icon(Icons.remove_red_eye),
+                  color: Colors.blue,
+                  onPressed: _recognize,
+                  tooltip: 'Recognize',
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+
+  Future<void> _exportImage() async {
     if (_controller.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
@@ -73,15 +147,11 @@ class _HomePageState extends State<HomePage> {
       return;
     }
 
-    final Uint8List? data = await _controller.toPngBytes(
-      height: 1000,
-      width: 1000,
-    );
+    final data = await _controller.toPngBytes();
     if (data == null) return;
 
     if (!mounted) return;
     await _push(
-      // ignore: use_build_context_synchronously
       context,
       Scaffold(
         appBar: AppBar(
@@ -97,7 +167,7 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Future<void> exportSVG(BuildContext context) async {
+  Future<void> _exportSVG() async {
     if (_controller.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
@@ -133,90 +203,45 @@ class _HomePageState extends State<HomePage> {
         MaterialPageRoute(builder: (BuildContext context) => widget),
       );
 
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: const Text('Vertex AI Quickdraw!')),
-        body: Column(
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Signature(
-                  key: const Key('signature'),
-                  controller: _controller,
-                  backgroundColor: Colors.grey[300]!,
-                ),
-              ),
-            ),
-            Text(_controller.isEmpty
-                ? "Signature pad is empty"
-                : "Signature pad is not empty"),
-          ],
+  void _clear() => setState(() => _controller.clear());
+  void _undo() => setState(() => _controller.undo());
+  void _redo() => setState(() => _controller.redo());
+
+  Future<void> _recognize() async {
+    final image = await _controller.toPngBytes();
+    if (image == null) return;
+
+    final svg = _controller.toSVG();
+
+    final provider = VertexProvider(
+      generativeModel: FirebaseVertexAI.instance.generativeModel(
+        model: 'gemini-1.5-flash-002',
+        systemInstruction: Content.text(
+          'You are an expert in recognizing hand-drawn images. You will be '
+          'given an image of a hand-drawn figure and you will recognize it.'
+          'Your response should be the name of the object in the image.'
+          'The choices will be from the following list: '
+          'apple, banana, cat, dog, elephant, fish, horse, lion, monkey, '
+          'orange, pear, pineapple, strawberry, tiger, watermelon',
         ),
-        bottomNavigationBar: BottomAppBar(
-          child: Container(
-            decoration: const BoxDecoration(color: Colors.black),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                //SHOW EXPORTED IMAGE IN NEW ROUTE
-                IconButton(
-                  key: const Key('exportPNG'),
-                  icon: const Icon(Icons.image),
-                  color: Colors.blue,
-                  onPressed: () => exportImage(context),
-                  tooltip: 'Export Image',
-                ),
-                IconButton(
-                  key: const Key('exportSVG'),
-                  icon: const Icon(Icons.share),
-                  color: Colors.blue,
-                  onPressed: () => exportSVG(context),
-                  tooltip: 'Export SVG',
-                ),
-                IconButton(
-                  icon: const Icon(Icons.undo),
-                  color: Colors.blue,
-                  onPressed: () {
-                    setState(() => _controller.undo());
-                  },
-                  tooltip: 'Undo',
-                ),
-                IconButton(
-                  icon: const Icon(Icons.redo),
-                  color: Colors.blue,
-                  onPressed: () {
-                    setState(() => _controller.redo());
-                  },
-                  tooltip: 'Redo',
-                ),
-                //CLEAR CANVAS
-                IconButton(
-                  key: const Key('clear'),
-                  icon: const Icon(Icons.clear),
-                  color: Colors.blue,
-                  onPressed: () {
-                    setState(() => _controller.clear());
-                  },
-                  tooltip: 'Clear',
-                ),
-                // STOP Edit
-                IconButton(
-                  key: const Key('stop'),
-                  icon: Icon(
-                    _controller.disabled ? Icons.pause : Icons.play_arrow,
-                  ),
-                  color: Colors.blue,
-                  onPressed: () {
-                    setState(
-                        () => _controller.disabled = !_controller.disabled);
-                  },
-                  tooltip: _controller.disabled ? 'Pause' : 'Play',
-                ),
-              ],
-            ),
-          ),
-        ),
-      );
+      ),
+    );
+
+    await File('/Users/csells/Downloads/image.png').writeAsBytes(image);
+    // await File('/Users/csells/Downloads/image.svg').writeAsString(svg.);
+
+    final attachment = ImageFileAttachment(
+      name: 'image.png',
+      mimeType: 'image/png',
+      bytes: image,
+    );
+
+    final stream = provider.generateStream(
+      'what is the attached image?',
+      attachments: [attachment],
+    );
+
+    final response = await stream.join();
+    debugPrint(response);
+  }
 }
